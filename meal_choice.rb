c0f2bb13c556b55b25c958f313eaa22d7284c2eb
Ashley Vu <ashleyvuhoo@gmail.com>
2018-07-24 17:51:06 +0000

    automatically backed up by learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -12,6 +12,8 @@ end
 
 breakfast
 
+def lunc(food = "grilled cheese")
+  puts ""
 # # Call the methods with puts and your own arguments here. Like this:
 # puts snacks("Swedish fish")
 ## This returns "Any time, is the right time for Sweedish fish!"
